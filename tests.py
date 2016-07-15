3575bebb44e338dfe7faa023286a93c6c26be456
Paweł Fertyk <pawel.fertyk@gmail.com>
2016-07-15 23:09:12 +0200

    Add tests for card drawing

--- tests.py
+++ tests.py
@@ -1,7 +1,29 @@
 import unittest
 from unittest.mock import MagicMock
 
-from main import AtonCore
+from main import AtonCore, Player
+
+
+class PlayerTestCase(unittest.TestCase):
+    def test_draw_cards(self):
+        player = Player()
+        player.deck = [1, 2, 3, 4, 4, 3, 2, 1]
+
+        player.draw_cards()
+
+        self.assertEqual(player.hand, [1, 2, 3, 4])
+        self.assertEqual(player.deck, [4, 3, 2, 1])
+
+    def test_use_discard_as_deck_if_deck_is_too_small(self):
+        player = Player()
+        player.deck = [2, 3, 4]
+        player.discard = [1, 1, 1, 1, 1]
+
+        player.draw_cards()
+
+        self.assertEqual(player.hand, [2, 3, 4, 1])
+        self.assertEqual(player.discard, [])
+        self.assertEqual(player.deck, [1, 1, 1, 1])
 
 
 class AtonCoreTestCase(unittest.TestCase):
